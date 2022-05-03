c9253409e8fb55ca95551af3d6f4f1aa51ebd5f3
dandeville <dandeville26@gmail.com>
2022-05-02 23:07:44 -0500

    Modificaciones a la componente search

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,9 +2,6 @@
 import React from 'react'
 
 // Material UI
-import InputBase from '@mui/material/InputBase'
-
-// Material UI
 import SearchIcon from '@mui/icons-material/Search'
 
 import { styled, alpha } from '@mui/material/styles'
